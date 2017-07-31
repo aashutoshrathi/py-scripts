aea0d91da9e3de5374ce05dedd64a861cef40a37
Aashutosh Rathi <aashutoshrathi@gmail.com>
2017-07-31 20:39:40 +0530

    AddToSendTo Added

---
+++ AddToSendTo/README.md
@@ -0,0 +1,27 @@
+## AddToSendTo
+[<img src="https://image.flaticon.com/icons/svg/179/179699.svg" align="right" width="100">](#)
+[![forthebadge](http://forthebadge.com/images/badges/made-with-python.svg)](http://forthebadge.com)
+
+Start-up Programs are those which start as soon as computer boots up, this script will help you to add your favorite programs or self made scripts/folders to startup.
+
+Works with Windows Only. 
+
+## Requirements and Installation
+
+For this to work, you need to install few modules by following commands.
+
+| Module | Steps |
+|:--------------:|:----------------:|
+| winshell |<pre>pip install winshell</pre>|
+| os (already available mostly) |<pre>pip install os</pre>|
+| win32com |Install from an executable [here](https://drive.google.com/open?id=0B3LWQGcO8qcwV3ltMV9vTGZ5Nms)|
+
+and now compile simply as you execute a Python file.
+
+
+## How it Works ?
+
+![Demo](https://media.giphy.com/media/xT39DnZWnaTFJBCS3K/giphy.gif)
+
+
+<p align="center"> Made with ❤ by <a href="https://github.com/aashutoshrathi">Aashutosh Rathi</a></p>
---
+++ AddToSendTo/add_to_sendTo.py
@@ -0,0 +1,71 @@
+"""
+Scripts written By : Aashutosh Rathi
+Credits : Stack Overflow and ↑ .
+
+"""
+
+
+import os
+import time
+import winshell
+import shutil
+from win32com.client import Dispatch
+
+
+def remove_quotes(string):
+    """
+    This function is used here to remove quotes from
+    paths used in this script.
+
+    :param string: Path with quotes.
+    :return: Path without quotes.
+    """
+    if string.startswith('"'):
+        string = string[1:]
+
+    if string.endswith('"'):
+        string = string[:-1]
+    return string
+
+
+def find_symbol(path):
+    for x in range(len(path)-1):
+        if path[x] == "\\":
+            result = x
+    return result
+
+
+def make_working_dir(file_path):
+    sym = find_symbol(file_path)
+    return file_path[:sym]
+
+
+def make_shortcut(file_path, dir_path, name):
+    you = os.getlogin()
+    startup = os.path.join('C:\\Users', you, 'AppData', 'Roaming',
+                            'Microsoft', 'Windows', 'SendTo')
+    name = name + '.lnk'
+    path = os.path.join(startup, name)
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = file_path
+    shortcut.WorkingDirectory = dir_path
+    shortcut.IconLocation = file_path
+    shortcut.save()
+
+
+def main():
+    print("\n===== Add Your files or Folders to SendTo in easy way =====")
+    you = os.getlogin()
+    print('Hey!', you)
+    file = input('Path of folder to be added to SendTo : ')
+    workingDir = make_working_dir(file)
+    file = remove_quotes(file)
+    workingDir = remove_quotes(workingDir)
+    name = input('Folder Name : ')
+    make_shortcut(file, workingDir, name)
+    print(name, 'added to SendTo Successfully !!')
+
+
+if __name__ == '__main__':
+    main()
